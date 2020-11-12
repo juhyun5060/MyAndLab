da0445821287f23faffccc4cafc4b917f5609d71
Juhyun <s2019w09@e-mirim.hs.kr>
2020-11-12 20:54:55 +0900

    5장 레이아웃

--- app/src/main/java/kr/hs/emirim/s2019w09/myandlab/ch06advwidget/DateTimeBookActivity.kt
+++ app/src/main/java/kr/hs/emirim/s2019w09/myandlab/ch06advwidget/DateTimeBookActivity.kt
@@ -1,21 +1,26 @@
 package kr.hs.emirim.s2019w09.myandlab.ch06advwidget
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
+import android.util.Log
 import android.view.View
 import android.widget.*
 import kr.hs.emirim.s2019w09.myandlab.R
 
+lateinit var chrono : Chronometer
+lateinit var btnStart : Button
+lateinit var btnEnd : Button
+lateinit var rdoCal : RadioButton
+lateinit var rdoTime : RadioButton
+lateinit var calView : CalendarView
+lateinit var tPicker: TimePicker
+lateinit var tvDateTime : TextView
+
 class DateTimeBookActivity : AppCompatActivity() {
-    
-    lateinit var chrono : Chronometer
-    lateinit var btnStart : Button
-    lateinit var btnEnd : Button
-    lateinit var rdoCal : RadioButton
-    lateinit var rdoTime : RadioButton
-    lateinit var calView : CalendarView
-    lateinit var tPicker: TimePicker
-    lateinit var tvDateTime : TextView
+
+    val TAG = "DateTimeBookActivity"
 
     var selectYear : Int = 0
     var selectMonth : Int = 0
@@ -38,6 +43,40 @@ class DateTimeBookActivity : AppCompatActivity() {
 
         tPicker.visibility = View.GONE
         calView.visibility = View.GONE
+
+        rdoTime.setOnClickListener {
+            calView.visibility = View.GONE
+            tPicker.visibility = View.VISIBLE
+            Log.d(TAG, "rdoTime 클릭됨")
+        }
+
+        rdoCal.setOnClickListener {
+            calView.visibility = View.VISIBLE
+            tPicker.visibility = View.GONE
+            Log.d(TAG, "rdoCal 클릭됨")
+        }
+
+        btnStart.setOnClickListener {
+            chrono.base = SystemClock.elapsedRealtime()
+            chrono.start()
+            chrono.setTextColor(Color.RED)
+            Log.d(TAG, "btnStart 클릭됨")
+        }
+
+        btnEnd.setOnClickListener {
+            chrono.stop()
+            chrono.setTextColor(Color.BLUE)
+            Log.d(TAG, "btnEnd 클릭됨")
+
+            tvDateTime.text = "" + selectYear + "년 " + "" + selectMonth + "월 " + selectDay + "일" + "" + tPicker.hour + "시 " + tPicker.minute + "분 예약됨"
+        }
+
+        calView.setOnDateChangeListener { view, year, month, dayOfMonth ->
+            selectYear = year
+            selectMonth = month + 1
+            selectDay = dayOfMonth
+            Log.d(TAG, "오늘은 : " + selectYear + "/" + selectMonth + "/" + selectDay)
+        }
 
     }
 }
